1d10b8618e9ac89a45b14348835ccc42d85a2e63
ZLoganZ <ln26805@gmail.com>
2023-12-19 09:40:14 +0700

    Update DOMAIN_NAME constant to use environment variable

--- src/util/constants/SettingSystem.ts
+++ src/util/constants/SettingSystem.ts
@@ -1,4 +1,4 @@
-export const DOMAIN_NAME = 'http://localhost:4052/api/v1';
+export const DOMAIN_NAME = import.meta.env.VITE_SERVER_ENDPOINT;
 
 export const USER_LOGIN = 'user_login';
 export const ID_USER = '_id';
